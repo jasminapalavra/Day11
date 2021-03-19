d479d5b82dff4935b9431f98e6f0c5ff03dae04b
jasminapalavra <jasmine.palavra@gmail.com>
2021-03-19 10:57:28 +0100

    Added ex04 src and index.js

---
+++ ex04/src/index.js
@@ -0,0 +1,16 @@
+
+var MonitorsListArray = ["Apple", "Peach", "Berry"];
+
+var monitorsList = []
+for (int i=0; i<arr.length; i++) {
+    for (int j=0 i<arr.length; j++) {
+}
+function myMonitorsFunction(arr) {
+var newMonitorsList = [...arr];
+    newMonitorsList.push(newMonitorsList);
+
+return myMonitorsFunction;
+}
+
+console.log(myMonitorsFunction(monitorsList));
+module.exports = myArrayFunction;
